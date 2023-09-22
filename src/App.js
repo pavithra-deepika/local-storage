7f124375c820c11b41b85b64b6470ef6596f55bc
pavithra <pavithradeepika301@gmail.com>
2023-09-22 23:09:58 +0530

    router install and create a rout path.

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
 import React from 'react';
 import './App.css';
+import Dashboard from './Components/Dashboard';
+import Login from './Components/Login';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
 
 function App() {
-  // localStorage.setItem('id', 101)
   return (
-    <div>
-      <p> Local Storage</p>
-      <div>
-        <p>Local storage {localStorage.getItem('id')}</p>
-      </div>
+    <div className='app'>
+      <h1>Home</h1>
+    <BrowserRouter>
+    <Routes>
+      <Route path='/dashboard' element={<Dashboard />} />
+      <Route path='/login' element={<Login />} />
+    </Routes>
+    </BrowserRouter>
     </div>
   )
 }
---
+++ src/Components/Login.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function Login() {
+  return (
+    <div>
+        <form>
+            <input type='text' />
+            <input type='password' />
+            <input type='submit' value='login' />
+        </form>
+    </div>
+  )
+}
+
+export default Login
