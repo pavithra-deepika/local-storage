ec05f1dc610cbf60dce1545b7cb1e39e58cfe2be
pavithra <pavithradeepika301@gmail.com>
2023-09-23 20:35:57 +0530

    create login page . save the data in localstorage

--- src/Components/Dashboard.js
+++ src/Components/Dashboard.js
@@ -1,8 +1,19 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useNavigate } from 'react-router-dom'
 
 function Dashboard() {
+  const navigate = useNavigate()
+  useEffect( () =>  {
+
+    if(!!! localStorage.getItem("isAuthenticated", true)){
+      navigate('/login')
+      
+    }
+
+  },[navigate])
+  // !!! used to suppose define , undefine pannama iruntha athu true, false matha use pannurom
   return (
-    <div>Welcome to Dashboard</div>
+    <div>Welcome to User</div>
   )
 }
 
--- src/Components/Login.js
+++ src/Components/Login.js
@@ -1,11 +1,24 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useNavigate } from 'react-router-dom';
 
 function Login() {
+    const [user, setUser] = useState("");
+    const [pass, setPass] = useState("")
+    const navigate = useNavigate()
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        if(user === "test" && pass === "test"){
+          localStorage.setItem("isAuthenticated", true);
+          navigate("/dashboard")
+        }
+
+
+    }
   return (
     <div>
-        <form>
-            <input type='text' />
-            <input type='password' />
+        <form onSubmit={(e) => handleSubmit(e)}>
+            <input type='text' onChange={(e) => setUser(e.target.value)}/>
+            <input type='password' onChange={(e) => setPass(e.target.value)}/>
             <input type='submit' value='login' />
         </form>
     </div>
